69fa3d5ed2345a996fc8b7d18c82bbee624d92f2
cloudswyss93 <cloudswyss93@gmail.com>
2025-10-29 17:41:11 +0100

    changes

--- src/components/mobile/ContactMobile.tsx
+++ src/components/mobile/ContactMobile.tsx
@@ -1,6 +1,6 @@
 import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
-import ContactParticleSystem from './ContactParticleSystem';
+import ContactParticleSystem from './ContactParticleSystemMobile';
 
 const ContactMobile: React.FC = () => {
   const form = useRef<HTMLFormElement>(null);
